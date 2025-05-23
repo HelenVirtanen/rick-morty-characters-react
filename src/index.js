26cd36793f369488f06e279ae8054e2b574a9565
Helen Virtanen <helenvirtanen28@gmail.com>
2025-05-23 17:59:57 +0300

    edited paths in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import { DataProvider } from './components';
+import { DataProvider } from './components/providers/DataProvider';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
